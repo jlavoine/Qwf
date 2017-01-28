5ef094c9c6c52da61d1d6f3cd25d3ffe48c9a9ef
jlavoine <joe.lavoine@gmail.com>
2017-01-28 17:45:41 -0500

    New view for setting the color of an outline

---
+++ Assets/MyLibrary/Properties/New/SetOutlineColorView.cs
@@ -0,0 +1,25 @@
+﻿using UnityEngine;
+using UnityEngine.UI;
+
+namespace MyLibrary {
+    public class SetOutlineColorView : PropertyView {
+        private Outline mOutline;
+        public Outline Outline {
+            get {
+                if ( mOutline == null ) {
+                    mOutline = GetComponent<Outline>();
+                }
+
+                return mOutline;
+            }
+        }
+
+        public override void UpdateView() {
+            Color color = GetValue<Color>();
+
+            if ( Outline != null ) {
+                Outline.effectColor = color;
+            }
+        }
+    }
+}
