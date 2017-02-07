3c3e8ee04b7c96cb7a31024cc6501a6c2e1820e3
jlavoine <joe.lavoine@gmail.com>
2017-02-07 08:38:40 -0500

    ResetMoves view/pm/test -- no functionality yet

---
+++ Assets/Qwf/Client/Scripts/Game/ResetMoves/Editor/TestResetMovesPM.cs
@@ -0,0 +1,13 @@
+﻿using NUnit.Framework;
+using NSubstitute;
+using UnityEngine;
+using MyLibrary;
+
+#pragma warning disable 0219
+
+namespace Qwf.Client {
+    [TestFixture]
+    public class TestResetMovesPM {
+
+    }
+}
---
+++ Assets/Qwf/Client/Scripts/Game/ResetMoves/ResetMovesView.cs
@@ -0,0 +1,11 @@
+﻿using MyLibrary;
+
+namespace Qwf.Client {
+    public class ResetMovesView : GroupView {
+
+       void Start() {
+            ResetMovesPM pm = new ResetMovesPM();
+            Init( pm.ViewModel );
+        }
+    }
+}
