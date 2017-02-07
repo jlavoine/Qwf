ec802a6709db09561f93c787d57dae0e0efd562f
jlavoine <joe.lavoine@gmail.com>
2017-02-06 21:46:58 -0500

    Attempting the move from dropping onto a slot view

--- Assets/Qwf/Client/Scripts/Game/GamePieceSlotView.cs
+++ Assets/Qwf/Client/Scripts/Game/GamePieceSlotView.cs
@@ -1,12 +1,15 @@
-﻿using System;
+﻿using UnityEngine;
 using MyLibrary;
 using UnityEngine.EventSystems;
 
 namespace Qwf.Client {
     public class GamePieceSlotView : GroupView, IDropHandler {
+        private GamePieceSlotPM mPM;
+
         public GamePieceView GamePieceInSlotView;
 
         public void Init( GamePieceSlotPM i_pm ) {
+            mPM = i_pm;
             SetModel( i_pm.ViewModel );
             InitGamePieceInSlotView( i_pm );
         }
@@ -15,8 +18,14 @@ namespace Qwf.Client {
             GamePieceInSlotView.Init( i_pm.GamePieceInSlot );
         }
 
-        public void OnDrop( PointerEventData eventData ) {
-            UnityEngine.Debug.LogError( "Dropped on me" );
+        public void OnDrop( PointerEventData i_eventData ) {
+            GameObject droppedObject = i_eventData.pointerDrag;
+            PlayerHandGamePieceView pieceView = droppedObject.GetComponent<PlayerHandGamePieceView>();
+            if ( pieceView != null ) {                
+                IPlayerHandGamePiecePM piecePM = pieceView.PM;
+                mPM.AttemptToPlayPieceInSlot( pieceView.PM );
+                UnityEngine.Debug.LogError( "Can the incoming piece be placed: " + mPM.Slot.CanPlacePieceIntoSlot( piecePM.GamePiece ) );
+            }
         }
     }
 }
