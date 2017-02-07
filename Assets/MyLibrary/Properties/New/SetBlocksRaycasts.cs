823d2aa770760b19a47e49692bc6c5433931f6cc
jlavoine <joe.lavoine@gmail.com>
2017-02-06 21:13:57 -0500

    Adding new view to block raycasts on canvas group

---
+++ Assets/MyLibrary/Properties/New/SetBlocksRaycasts.cs
@@ -0,0 +1,28 @@
+﻿using UnityEngine;
+using UnityEngine.UI;
+
+namespace MyLibrary {
+    public class SetBlocksRaycasts : PropertyView {
+        private CanvasGroup mCanvasGroup;
+        public CanvasGroup CanvasGroup {
+            get {
+                if ( mCanvasGroup == null ) {
+                    mCanvasGroup = GetComponent<CanvasGroup>();
+                }
+
+                return mCanvasGroup;
+            }
+        }
+
+        public override void UpdateView() {
+            bool state = GetValue<bool>();            
+
+            if ( CanvasGroup != null ) {
+                CanvasGroup.blocksRaycasts = state;
+            }
+            else {
+                MyMessenger.Instance.Send<LogTypes, string, string>( MyLogger.LOG_EVENT, LogTypes.Error, "No CanvasGroup element for SetBlocksRaycasts: " + PropertyName, "UI" );
+            }
+        }
+    }
+}
