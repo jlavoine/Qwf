87b742b9aa539a64c9a404a94bcdd3b5cf5a29b6
jlavoine <joe.lavoine@gmail.com>
2017-02-06 20:33:27 -0500

    Adding the game manager to playground

---
+++ Assets/Qwf/Client/Scripts/Test/TestGameWithManager.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+namespace Qwf.Client {
+    public class TestGameWithManager : MonoBehaviour {
+        private ClientGameManager mManager;
+
+        // Use this for initialization
+        void Awake() {
+            mManager = new ClientGameManager();
+        }
+
+        void OnDestroy() {
+            mManager.Dispose();
+        }
+
+        // Update is called once per frame
+        void Update() {
+
+        }
+    }
+}
