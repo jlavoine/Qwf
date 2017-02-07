dde1896b02f5c063431f024cc429d6a6fb109aae
jlavoine <joe.lavoine@gmail.com>
2017-02-07 11:11:43 -0500

    SendMoves visible false by default

--- Assets/Qwf/Client/Scripts/Game/EditMoves/Editor/TestSendMovesPM.cs
+++ Assets/Qwf/Client/Scripts/Game/EditMoves/Editor/TestSendMovesPM.cs
@@ -8,5 +8,11 @@ using MyLibrary;
 namespace Qwf.Client {
     [TestFixture]
     public class TestSendMovesPM : QwfUnitTest {
+        [Test]
+        public void WhenCreatingPM_IsVisibleProperty_FalseByDefault() {
+            SendMovesPM systemUnderTest = new SendMovesPM();
+
+            Assert.AreEqual( 0f, systemUnderTest.ViewModel.GetPropertyValue<float>( PassTurnPM.VISIBLE_PROPERTY ) );
+        }
     }
 }
--- Assets/Qwf/Client/Scripts/Game/EditMoves/SendMovesPM.cs
+++ Assets/Qwf/Client/Scripts/Game/EditMoves/SendMovesPM.cs
@@ -2,9 +2,19 @@
 
 namespace Qwf.Client {
     public class SendMovesPM : GenericViewModel {
+        public const string VISIBLE_PROPERTY = "IsVisible";
+
+        public SendMovesPM() {
+            SetVisibleProperty( false );
+        }
 
         public void Dispose() {
 
+        }
+
+        private void SetVisibleProperty( bool i_visible ) {
+            float fAlpha = i_visible ? 1f : 0f;
+            ViewModel.SetProperty( VISIBLE_PROPERTY, fAlpha );
         }
     }
 }
