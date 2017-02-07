c0163efe597224504c54874563c7a7fe59cf9ecf
jlavoine <joe.lavoine@gmail.com>
2017-02-07 11:09:44 -0500

    Making pass turn visible by default

--- Assets/Qwf/Client/Scripts/Game/EditMoves/Editor/TestPassTurnPM.cs
+++ Assets/Qwf/Client/Scripts/Game/EditMoves/Editor/TestPassTurnPM.cs
@@ -8,5 +8,11 @@ using MyLibrary;
 namespace Qwf.Client {
     [TestFixture]
     public class TestPassTurnPM : QwfUnitTest {
+        [Test]
+        public void WhenCreatingPM_IsVisibleProperty_TrueByDefault() {
+            PassTurnPM systemUnderTest = new PassTurnPM();
+
+            Assert.AreEqual( 1f, systemUnderTest.ViewModel.GetPropertyValue<float>( PassTurnPM.VISIBLE_PROPERTY ) );
+        }
     }
 }
--- Assets/Qwf/Client/Scripts/Game/EditMoves/PassTurnPM.cs
+++ Assets/Qwf/Client/Scripts/Game/EditMoves/PassTurnPM.cs
@@ -2,9 +2,19 @@
 
 namespace Qwf.Client {
     public class PassTurnPM : GenericViewModel {
+        public const string VISIBLE_PROPERTY = "IsVisible";
+
+        public PassTurnPM() {
+            SetVisibleProperty( true );
+        }
 
         public void Dispose() {
 
+        }
+
+        private void SetVisibleProperty( bool i_visible ) {
+            float fAlpha = i_visible ? 1f : 0f;
+            ViewModel.SetProperty( VISIBLE_PROPERTY, fAlpha );
         }
     }
 }
