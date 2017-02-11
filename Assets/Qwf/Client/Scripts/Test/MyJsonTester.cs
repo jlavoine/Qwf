ba0cfe5709fd9e1efd5cbb66d282cfad726ddade
jlavoine <joe.lavoine@gmail.com>
2017-02-11 18:36:17 -0500

    Some failed json testing

--- Assets/Qwf/Client/Scripts/Test/MyJsonTester.cs
+++ Assets/Qwf/Client/Scripts/Test/MyJsonTester.cs
@@ -2,84 +2,92 @@
 using System.Collections.Generic;
 using UnityEngine;
 using Newtonsoft.Json;
-using Qwf;
+using System.Runtime.Serialization;
 
-public class MyJsonTester : MonoBehaviour {
+namespace Qwf {
+    public class MyJsonTester : MonoBehaviour {
 
-	// Use this for initialization
-	void Start () {
-        /*PlayerDeckData test = new PlayerDeckData();
-        test.GamePieces = new List<DeckGamePieceData>();
-        DeckGamePieceData one = new DeckGamePieceData();
-        one.PieceType = 0;
-        one.PieceValueToCount = new Dictionary<int, int>() { { 0, 1 }, { 3, 2 } };
+        // Use this for initialization
+        void Start() {
+            /*PlayerDeckData test = new PlayerDeckData();
+            test.GamePieces = new List<DeckGamePieceData>();
+            DeckGamePieceData one = new DeckGamePieceData();
+            one.PieceType = 0;
+            one.PieceValueToCount = new Dictionary<int, int>() { { 0, 1 }, { 3, 2 } };
 
-        DeckGamePieceData two = new DeckGamePieceData();
-        two.PieceType = 1;
-        two.PieceValueToCount = new Dictionary<int, int>() { { 0, 1 }, { 3, 2 } };
+            DeckGamePieceData two = new DeckGamePieceData();
+            two.PieceType = 1;
+            two.PieceValueToCount = new Dictionary<int, int>() { { 0, 1 }, { 3, 2 } };
 
-        test.GamePieces.Add( one );
-        test.GamePieces.Add( two );
+            test.GamePieces.Add( one );
+            test.GamePieces.Add( two );
 
-        string json = JsonConvert.SerializeObject( test );
-        UnityEngine.Debug.LogError( json );
-        */
+            string json = JsonConvert.SerializeObject( test );
+            UnityEngine.Debug.LogError( json );
+            */
 
-        //TestInterfaces();
+            //TestInterfaces();
 
-        TestObstacleData();
-    }
+            //TestObstacleData();
 
-    private void TestObstacleData() {
-        List<GameObstacleData> list = new List<GameObstacleData>();
-        GameObstacleData one = new GameObstacleData();
-        one.FinalBlowValue = 3;
-        one.SlotsData = new List<GamePieceSlotData>();
+            TestInterfaces2();
+        }
 
-        GamePieceSlotData slotOne = new GamePieceSlotData();
-        slotOne.PieceType = 1;
-        slotOne.ScoreValue = 1;
+        private void TestObstacleData() {
+            List<GameObstacleData> list = new List<GameObstacleData>();
+            GameObstacleData one = new GameObstacleData();
+            one.FinalBlowValue = 3;
+            one.SlotsData = new List<GamePieceSlotData>();
 
-        GamePieceSlotData slot2 = new GamePieceSlotData();
-        slot2.PieceType = 3;
-        slot2.ScoreValue = 1;
+            GamePieceSlotData slotOne = new GamePieceSlotData();
+            slotOne.PieceType = 1;
+            slotOne.ScoreValue = 1;
 
-        one.SlotsData.Add( slotOne );
-        one.SlotsData.Add( slot2 );
+            GamePieceSlotData slot2 = new GamePieceSlotData();
+            slot2.PieceType = 3;
+            slot2.ScoreValue = 1;
 
-        GameObstacleData two = new GameObstacleData();
-        two.FinalBlowValue = 2;
-        two.SlotsData = new List<GamePieceSlotData>();
-        two.SlotsData.Add( slotOne );
+            one.SlotsData.Add( slotOne );
+            one.SlotsData.Add( slot2 );
 
-        list.Add( one );
-        list.Add( two );
+            GameObstacleData two = new GameObstacleData();
+            two.FinalBlowValue = 2;
+            two.SlotsData = new List<GamePieceSlotData>();
+            two.SlotsData.Add( slotOne );
 
-        string json = JsonConvert.SerializeObject( list );
-        UnityEngine.Debug.LogError( json );
-    }
+            list.Add( one );
+            list.Add( two );
 
-    private void TestInterfaces() {
-        GameObstaclesUpdate obstacles = new GameObstaclesUpdate();
-        obstacles.Obstacles = new List<GameObstacleUpdate>();
+            string json = JsonConvert.SerializeObject( list );
+            UnityEngine.Debug.LogError( json );
+        }
 
-        GameObstacleUpdate one = new GameObstacleUpdate();
-        one.Id = "Goblin";        
+        private void TestInterfaces() {
+            GameObstaclesUpdate obstacles = new GameObstaclesUpdate();
+            obstacles.Obstacles = new List<GameObstacleUpdate>();
 
-        GameObstacleUpdate two = new GameObstacleUpdate();
-        two.Id = "Blob";
+            GameObstacleUpdate one = new GameObstacleUpdate();
+            one.Id = "Goblin";
 
-        obstacles.Obstacles.Add( one );
-        obstacles.Obstacles.Add( two );
+            GameObstacleUpdate two = new GameObstacleUpdate();
+            two.Id = "Blob";
 
-        string json = JsonConvert.SerializeObject( obstacles );
-        UnityEngine.Debug.LogError( json );
+            obstacles.Obstacles.Add( one );
+            obstacles.Obstacles.Add( two );
 
-        GameObstaclesUpdate deserial = JsonConvert.DeserializeObject<GameObstaclesUpdate>( json );
-        IGameObstacleUpdate update1 = deserial.GetUpdate( 0 );
-        UnityEngine.Debug.LogError( update1.GetId() );
+            string json = JsonConvert.SerializeObject( obstacles );
+            UnityEngine.Debug.LogError( json );
 
-        IGameObstacleUpdate update2 = deserial.GetUpdate( 1 );
-        UnityEngine.Debug.LogError( update2.GetId() );
+            GameObstaclesUpdate deserial = JsonConvert.DeserializeObject<GameObstaclesUpdate>( json );
+            IGameObstacleUpdate update1 = deserial.GetUpdate( 0 );
+            UnityEngine.Debug.LogError( update1.GetId() );
+
+            IGameObstacleUpdate update2 = deserial.GetUpdate( 1 );
+            UnityEngine.Debug.LogError( update2.GetId() );
+        }
+
+        private void TestInterfaces2() {
+
+        }
     }
 }
